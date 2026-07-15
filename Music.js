d82513699222427986060f81d8d4bebf3f751f17
urooj <uroojismail48@gmail.com>
2026-07-16 03:35:35 +0500

    Made-somethingSpecial

---
+++ Music.js
@@ -0,0 +1,581 @@
+const songs = [
+  {
+    title: "Zaalima",
+    msg: "jo Apley khatir trpe Pehle se e kya usse trpana oh zaalimaa 👀💋 ",
+    image: "./ings/pic2.jpg"  ,
+    src: "./songs/For her/Zaalima.m4a"
+  },
+
+
+ 
+  {
+    title: "Khuda Bhi",
+    msg: "Ap bemisal ho 😭💕🦋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Khuda Bhi.m4a"
+  },
+  {
+    title: "Deewana Kar Raha Hai",
+    msg: "ab btayein jayein tou jayein khn 😭🦋💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Deewana kar raha hai.m4a"
+  },
+  {
+    title: "Dekha Tenu",
+    msg: "dekha ap nu😭😭(punjabi nh ati),aesa nasha charade dholna",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Dekhha Tenu From Mr And Mrs Mahi.m4a"
+  },
+  {
+    title: "Haan Tu Hain",
+    msg: "jo khwabo khhyalo me soucha nh tha apne mujhe itna pyar dia😭💋💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Haan Tu Hain.m4a"
+  },
+  {
+    title: "Maula Mere Maula",
+    msg: "Ankheinn aaapkiiii👀💕",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Maula Mere Maula.m4a"
+  },
+  {
+    title: "Ishq Mubarak",
+    msg: "ap hi ap ho jo har taraf mere🦋💗-",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/ISHQ MUBARAK.m4a"
+  },
+  {
+    title: "Chand Chupa Badal Mein",
+    msg: "mera chand chupa lahore me😭(sad loif)",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Chand Chupa Badal Mein.m4a"
+  },
+  {
+    title: "Kya Baat Aye",
+    msg: "kya bat hai(😋😜👅)",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Kya Baat Ay.m4a"
+  },
+  {
+    title: "Maahi",
+    msg: "Aja mere khwb saja ja re😭❤",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Maahi.m4a"
+  },
+  {
+    title: "Until I Found You",
+    msg: "Till i found her👀💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Until I Found You.m4a"
+  },
+  {
+    title: "Main Agar Kahoon",
+    msg: "tumsa haseen kaynat me nh hai kahin😭❤",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Main Agar Kahoon.m4a"
+  },
+  {
+    title: "Tumse Milke Dil Ka Hai Jo Haal",
+    msg: "HaAAAYEEE tumse mil k dil ka hai jo haal",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Tumse Milke Dil Ka From Main Hoon Na.m4a"
+  },
+  {
+    title: "Tere Mast Mast Do Nain",
+    msg: "mera dil ka legaaye chain👀💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Tere Mast Mast Do Nain.m4a"
+  },
+  {
+    title: "Main Rang Sharbaton Ka",
+    msg: "me rng sharbatoon ka AP Neel ghat ka pani(TF is this lyrics)",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Main Rang Sharbaton Ka.m4a"
+  },
+  {
+    title: "Suno Na Sangemarmar",
+    msg: "dil pe mere raj tumhara😭💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Suno- Na Sangemarmar From Youngistaan.m4a"
+  },
+  {
+    title: "Tum Hi Ho",
+    msg: "TUM HI HO💗🦋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Tum Hi Ho - Arijit Singh.m4a"
+  },
+  {
+    title: "Toota Jo Kabhi Tara",
+    msg: "ap milya tou janey na donga mein😭💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Toota Jo Kabhi Tara.m4a"
+  },
+  {
+    title: "Galliyan",
+    msg: "ummmahhh💋💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Galliyan.m4a"
+  },
+  {
+    title: "Meri Zindagi Hai Tu",
+    msg: "Qurban tujhpe sb mera😭💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Meri Zindagi Hai Tu.m4a"
+  },
+  {
+    title: "Sanam Re",
+    msg: "sanam reeeee💋💋💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Sanam Re - Instrumental.m4a"
+  },
+  {
+    title: "Sitaare",
+    msg: "bs tumse milne ki dair thi😭💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Sitaare From Ikkis.m4a"
+  },
+  {
+    title: "Ishq Wala Love",
+    msg: "surkh wala sooz wala wese wala LOVE💗",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/SOTYAlia Bhatt,Sidharth Malhotra,Varun DhawanNeeti Mohan.m4a"
+  },
+  {
+    title: "Tu Hi Haqeeqat",
+    msg: "me hi tou apkey dil me hun me hi tou sanson me basoon ",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Tu Hi Haqeeqat.m4a"
+  },
+  {
+    title: "Aisa Banna Sawarna",
+    msg: "😭💋💋💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Aisa Banna Sawarna Mubarak Tumhen.m4a"
+  },
+  {
+    title: "Afreen Afreen",
+    msg: "jo b dekhe agar tou kahe hum nashein😭💋🦋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Afreen Afreen.m4a"
+  },
+  {
+    title: "Perfect",
+    msg: "found a love beatiful and sweet👀❤",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Perfect.m4a"
+  },
+  {
+    title: "Chand Sifarish",
+    msg: "💕💕💕",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Chand Sifarish.m4a"
+  },
+  {
+    title: "Dekha Hazaro Dafaa",
+    msg: "smbhale se smbhlte nh hain hum kch ap me bt aesi hai👀💕",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Dekha Hazaro Dafaa.m4a"
+  },
+  {
+    title: "Dhadak",
+    msg: "jurh ske beech me kahin tarap hain na?👀",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Dhadak Title Track.m4a"
+  },
+  {
+    title: "Finding Her",
+    msg: "sapno se jata nh😭🦋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Finding Her.m4a"
+  },
+    {
+    title: "Ajab si",
+    msg: "chahe jisse dur se duniya woh mere kareeb hai😭💋💕",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Ajab Si.m4a"
+  },  {
+    title: "Gerua",
+    msg: "rng de tu mahe gerua(not sure what this means😭😭)",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Gerua Slowed.m4a"
+    },
+      {
+    title: "Barish",
+    msg: "yeh milne ki khwahish yeh khwahish purani😭💋",
+    image: "./ings/pic2.jpg" ,
+    src: "./songs/For her/Baarish.m4a"
+  }
+];
+const fromME = [{
+    
+      
+    title: "be inteha",
+    msg: "koi kasar na rhe 😭💋chahun tujhe is qdr be inteha",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Be Intehaan.m4a"
+  
+},
+
+{
+    
+      
+    title: "Dhun",
+    msg: "naam k chnd khwb hain jinko hi me ghr kheta rha💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Dhun Movie Saiyaara.m4a"
+  
+},
+{
+    
+      
+    title: "Dil",
+    msg: "😭😭😭😭💕💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Dil Shreya's Version.m4a"
+  
+},
+{
+    
+      
+    title: "Ehsaas",
+    msg: "ap ek tohfa ho khuda ka💋💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Ehsaas_Lyrics_-_Faheem_Abdullah,_Duha_Shah,_Vaibhav_Pani,_Hyder_Dar(256k).mp3"
+  
+},
+{
+    
+      
+    title: "Ek mulaqat",
+    msg: "zaroriiiii😭😭😭💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/EK Mulaqat.m4a"
+  
+},
+{
+    
+      
+    title: "Falling for u",
+    msg: "baby i have falling for u👀💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Fallin for You - Shrey Singhal.m4a"
+  
+},
+{
+    
+      
+    title: "Gehra howa ",
+    msg: "AP agar meri👀💋❤",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Gehra Hua.m4a"
+  
+},
+{
+    
+      
+    title: "Hale dil",
+    msg: "AP meri rah ka sitara💋💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Hale Dil.m4a"
+  
+},
+{
+    
+      
+    title: "Humsafar",
+    msg: "HUMSAFAR💋🦋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Humsafar.m4a"
+  
+},
+{
+    
+      
+    title: "I'll be waiting",
+    msg: "kbh JO BADAL BRSEEEE💋💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/I'll Be Waiting (Kabhi Jo Baadal) - Arjun, Arijit Singh.m4a"
+  
+},
+{
+    
+      
+    title: "JAhan tum ho",
+    msg: "Jo adat khwabon me milne ki wahi tum ho🦋💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Jahaan Tum Ho - Shrey Singhal.m4a"
+  
+},
+
+{
+    
+      
+    title: "Jo tum mere ho",
+    msg: "MERE HOO> tou me kch nh mangu duniya se(janu sad part hamare liye nh hai kisi b song ka)",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Jo Tum Mere Ho.m4a"
+  
+},
+{
+    
+      
+    title: "Kabhi na kbhi",
+    msg: "hoga jhn pe andhere ka sawera👀💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/KABHI NA KABHI.m4a"
+  
+},
+{
+    
+      
+    title: "Kaise hua",
+    msg: "kaiseee😭😭💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Kaise Hua.m4a"
+  
+},
+{
+    
+      
+    title: "Kangna tera Ni",
+    msg: "oh sohni tenu apna bnana🦋💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Kangana Tera Ni - sped up - IAMPRATHEEK.m4a"
+  
+},
+{
+    
+      
+    title: "Kaun tujhe",
+    msg: "kaun tujhe yun pyar krega jese me krti hun🦋💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Kaun Tujhe From MSDhoni - The Untold Story.m4a"
+  
+},
+{
+    
+      
+    title: "Khat",
+    msg: "apkey liye Ghr bnaun😭💕💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Khat.m4a"
+  
+},
+{
+    
+      
+    title: "Laboon ko",
+    msg: "😉💕💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Labon Ko.m4a"
+  
+},
+{
+    
+      
+    title: "Meherbaan",
+    msg: "kandhe pe sr jhukhaya jb💋💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Meherbaan.m4a"
+  
+},
+{
+    
+      
+    title: "Mujhko barsat",
+    msg: "mujhko brsat bna lo😭💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Mujhko Barsaat Bana Lo.m4a"
+  
+},
+{
+    
+      
+    title: "Muskurane",
+    msg: "muskurane ki wjah tum hoo💕💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Muskurane Romantic.m4a"
+  
+},
+{
+    
+      
+    title: "Girl you are mine",
+    msg: "me jhut kahun tou mar ja unnn💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Oh Girl You're Mine . - Tarun Sagar, Alyssa Mendonsa, Loy Mendonsa.m4a"
+  
+},
+{
+    
+      
+    title: "Pyari amanat",
+    msg: "pyari amanat💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Pyari Amaanat.m4a"
+  
+},
+{
+    
+      
+    title: "Ranjheya we",
+    msg: "😭💋💋🦋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Ranjheya Ve.m4a"
+  
+},
+{
+    
+      
+    title: "Samjhwan",
+    msg: "na tere bina lg da jee💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Samjhawan Unplugged by Alia Bhatt [From Humpty Sharma Ki Dulhania].m4a"
+  
+},
+{
+    
+      
+    title: "Shiddat",
+    msg: "chute kbh na woh adat bna loon 💕💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Shiddat Title Track.m4a"
+  
+},
+{
+    
+      
+    title: "Souch na skey",
+    msg: "me apko kitna chahta hun yeh ap kbh souch na skein💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Soch Na Sake.m4a"
+  
+},
+{
+    
+      
+    title: "Tera ban jaunga",
+    msg: "💕💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tera Ban Jaunga.m4a"
+  
+},
+{
+    
+      
+    title: "Tera hi bs hona chahun",
+    msg: "me tou bs jee rha hun bin mere😭😭💕💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tera Hi Bas Hona Chaahoon.m4a"
+  
+},
+{
+    
+      
+    title: "Tere vaaste",
+    msg: "rehna zara bchke 😉💋 ",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tere Vaaste (From  Zara Hatke Zara Bachke ) - Sachin-Jigar, Varun Jain, Shadab Faridi, Altamash Faridi, Amitabh Bhattacharya.m4a"
+  
+},
+{
+    
+      
+    title: "Tere liye",
+    msg: "thora chain mil jayga bahun me bhr le mujhe💋💋💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tere_Liye_-_Lyrical___Jhankar_____Atif_Aslam___Shreya_Ghoshal___Vivek_Oberoi___Prince(256k).mp3"
+  
+},
+{
+    
+      
+    title: "Teri yadein",
+    msg: "kbh toun pass mere ao🤗😉",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Teri Yaadein.m4a"
+  
+},
+{
+    
+      
+    title: "Tha krke",
+    msg: "thAAA krke😂😂",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tha Kar Ke - Pritam, Neeraj Shridhar, Anweshaa, Aakariti, Earl D'Souza, Indie.m4a"
+  
+},
+{
+    
+      
+    title: "Tu chahiye",
+    msg: "shamo subha 💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tu Chahiye.m4a"
+  
+},
+{
+    
+      
+    title: "Tu hai tou",
+    msg: "dil dharakta hai💋💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tu Hain Toh.m4a"
+  
+},
+{
+    
+      
+    title: "Tu jo hai ",
+    msg: "ap jo hain tou me hun💋💋",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tu Jo Hain From Mr X.m4a"
+  
+},
+{
+    
+      
+    title: "tujhe kitna chahein aur hum",
+    msg: "aaur b bht chahein hum💋💋💗",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tujhe Kitna Chahein Aur Film Version.m4a"
+  
+},
+{
+    
+      
+    title: "Tujhe souchta hun me",
+    msg: "isse ziyada tujhe aur chahun kya😭😭💕💕",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tujhe Sochta Hoon From Jannat 2.m4a"
+  
+},
+{
+    
+      
+    title: "Tu ne jo na kaha",
+    msg: "tu ne jo na kaha woh me sunta rha💗💗",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Tune Jo Na Kaha.m4a"
+  
+},
+{
+    
+      
+    title: "Zara sa",
+    msg: "zaara sa dillllll me de jgah tuu😭💕😉💋💗",
+    image:"./ings/pic2.jpg" ,
+    src: "./songs/From me/Zara Sa.m4a"
+  
+},
+{
+    
+      
+    title: "Zarorat",
+    msg: "mujhe apki zarorat hai💗💋💗💗",
+    image: "./ings/pic2.jpg",
+    src: "./songs/From me/Zaroorat - Rahul Jain.m4a"
+  
+},
+
+]
